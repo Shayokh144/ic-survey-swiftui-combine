7c7c655bd99e047554cd0c709de480e8ff73b4e2
Shayokh144 <shayokhbd144@gmail.com>
2022-10-31 23:08:03 +0700

    CD init

--- Survey/Survey/Sources/Application/SurveyApp.swift
+++ Survey/Survey/Sources/Application/SurveyApp.swift
@@ -5,10 +5,25 @@
 //  Created by Taher on 25/10/22.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+    }
+}
 
 @main
 struct SurveyApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             SplashView()
